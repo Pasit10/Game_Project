9067508be95a30ff12798735ffc17ab44358fc64
Pasit10 <pasit1006@gmail.com>
2023-02-23 00:46:14 +0700

    can move

--- Game.java
+++ Game.java
@@ -3,18 +3,17 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class Game extends JFrame implements KeyListener{
+public class Game extends JPanel implements ActionListener{
     private JPanel[][] scene = new JPanel[80][80];
     private ArrayList<snakeposXY> snake = new ArrayList<>();
-    private char command;
-    //private boolean canbrake;
+    private char command = 'd';
+    Timer t = new Timer(100, this);
     Game(){
-        setSize(1000,1500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(80,80));
-        addKeyListener(this);
+        addKeyListener(new KeyboardGame());
         GameComponent();
-        setVisible(true);
+        setFocusable(true);
+        t.start();
     }
 
     private void GameComponent(){
@@ -26,7 +25,6 @@ public class Game extends JFrame implements KeyListener{
                 add(scene[i][j]);
             }
         }
-
         // ------------------ TEST ------------------------//
         for(int i = 0;i < 20;i++){
             snake.add(new snakeposXY(10,10));
@@ -35,64 +33,54 @@ public class Game extends JFrame implements KeyListener{
     }
 
     private void move(char command){
-        //int x = 1;
-        //while(true){
-            JPanel snakeTail = snake.get(snake.size() - 1).pos;
-            for(int j = snake.size() - 1; j > 0;j--){
-                snake.get(j).row = snake.get(j - 1).row;
-                snake.get(j).col = snake.get(j - 1).col;
-                snake.get(j).pos = snake.get(j - 1).pos;
-            }
-            // --------------- set Pos --------------- //
-            if(snake.get(0).row - 1 < 0){
-                snake.get(0).row = scene.length;
-            }
-            if(snake.get(0).col - 1 < 0){
-                snake.get(0).col = scene[0].length;
-            }
-            switch(command){
-                case 'w' :
-                    snake.get(0).row = (snake.get(0).row - 1) % scene.length;
-                    break;
-                case 's':
-                    snake.get(0).row = (snake.get(0).row + 1) % scene.length;
-                    break;
-                case 'a':
-                    snake.get(0).col = (snake.get(0).col - 1) % scene[0].length;
-                    break;
-                case 'd':
-                    snake.get(0).col = (snake.get(0).col + 1) % scene[0].length;
-                    break;
-            }
-            snake.get(0).pos = scene[(snake.get(0).row) % scene.length][snake.get(0).col];
-            // --------------- set Pos --------------- //
-            // --------------- setcolor --------------//
-            snake.get(0).pos.setBackground(Color.green);
-            snakeTail.setBackground(Color.BLACK);
-            System.out.println(snake);
-             // --------------- setcolor --------------//
-            // if(canbrake){
-            //     break;
-            // }
-            //x--;
-        //}
+        JPanel snakeTail = snake.get(snake.size() - 1).pos;
+        for(int j = snake.size() - 1; j > 0;j--){
+            snake.get(j).row = snake.get(j - 1).row;
+            snake.get(j).col = snake.get(j - 1).col;
+            snake.get(j).pos = snake.get(j - 1).pos;
+        }
+        // --------------- set Pos --------------- //
+        if(snake.get(0).row - 1 < 0){
+            snake.get(0).row = scene.length;
+        }
+        if(snake.get(0).col - 1 < 0){
+            snake.get(0).col = scene[0].length;
+        }
+        switch(command){
+            case 'w' :
+                snake.get(0).row = (snake.get(0).row - 1) % scene.length;
+                break;
+            case 's':
+                snake.get(0).row = (snake.get(0).row + 1) % scene.length;
+                break;
+            case 'a':
+                snake.get(0).col = (snake.get(0).col - 1) % scene[0].length;
+                break;
+            case 'd':
+                snake.get(0).col = (snake.get(0).col + 1) % scene[0].length;
+                break;
+        }
+        snake.get(0).pos = scene[(snake.get(0).row) % scene.length][snake.get(0).col];
+        snake.get(0).pos.setBackground(Color.green);
+        snakeTail.setBackground(Color.BLACK);
+
     }
 
-    // -------------- KeyListener --------------- //
-    @Override public void keyPressed(KeyEvent e){
-        int keyPress = e.getKeyCode();
-        switch(keyPress){
-            case KeyEvent.VK_W: command = 'w';break;
-            case KeyEvent.VK_S: command = 's';break;
-            case KeyEvent.VK_A: command = 'a';break;
-            case KeyEvent.VK_D: command = 'd'; break;
-        }
-        System.out.println(command);
+    @Override public void actionPerformed(ActionEvent ev){
         move(command);
     }
-    @Override public void keyTyped(KeyEvent e) {}
-    @Override public void keyReleased(KeyEvent e) {}
-     // -------------- KeyListener --------------- //
+
+    private class KeyboardGame extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+            int keyPress = e.getKeyCode();
+            switch(keyPress){
+                case KeyEvent.VK_W: command = 'w';break;
+                case KeyEvent.VK_S: command = 's';break;
+                case KeyEvent.VK_A: command = 'a';break;
+                case KeyEvent.VK_D: command = 'd'; break;
+            }
+        }
+    }
 
     private class snakeposXY{
         int row , col;
@@ -106,7 +94,11 @@ public class Game extends JFrame implements KeyListener{
         }
     }
 
-    public static void main(String[] args){
-        new Game();
+    public static void main(String[] args) {
+        JFrame x = new JFrame();
+        x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        x.setSize(1000,1000);
+        x.add(new Game());
+        x.setVisible(true);
     }
 }
