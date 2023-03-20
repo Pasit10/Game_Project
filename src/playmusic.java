c1a3d39c2e440859184f3f00f4837f6e0b1b53c5
Dear <madear5659@gmail.com>
2023-03-20 09:16:36 +0700

    ลดเสียง

--- src/playmusic.java
+++ src/playmusic.java
@@ -3,6 +3,7 @@ import java.io.*;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 class playmusic {
     Clip clip;
@@ -15,6 +16,8 @@ class playmusic {
                 clip.open(audioinput);
                 clip.start();
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
+                FloatControl nyancat = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                nyancat.setValue(-10);
             }else{
                 System.out.println("can't play");
             }
